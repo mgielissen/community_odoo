5ce1f7b8e1dc2d93da9d22428735b6d891929ab5
Temur <tvibliani@users.noreply.github.com>
2021-01-04 00:01:42 +0400

    Refactor: update manifest
    - Make description in manifest comparable to the new module
      initialized by the `scaffold` CLI command of Odoo.

--- website_crm_captcha/__manifest__.py
+++ website_crm_captcha/__manifest__.py
@@ -1,9 +1,9 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Captcha - Contact Form',
+
     'summary': 'Simple Captcha for Contact Us Form',
-    'category': 'Website',
-    'license': 'LGPL-3',
-    'version': '14.0.1.0.0',
+
     'description': """
 Simple Captcha for "Contact Us" Form
 ====================================
@@ -32,11 +32,23 @@ or with
 
 
 .. _Captcha: https://github.com/lepture/captcha
-""",
+    """,
+
     'author': 'PluginMere',
     'website': 'https://github.com/pluginmere/community_odoo',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Website',
+    'license': 'LGPL-3',
+    'version': '14.0.1.0.0',
+
+    # any module necessary for this one to work correctly
     'depends': ['website_crm'],
     'external_dependencies': {'python': ['captcha']},
+
+    # always loaded
     'data': [
         'views/website_crm_captcha.xml',
         'views/snippets/s_website_form.xml',
